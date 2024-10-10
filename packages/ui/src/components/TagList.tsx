7e432741799640e6a7cb803848c78f2c1ef67168
hemahg <hhg@redhat.com>
2024-10-10 23:19:41 +0530

    Add inline text editor to edit the tag description
    Signed-off-by: hemahg <hhg@redhat.com>

--- packages/ui/src/components/TagList.tsx
+++ packages/ui/src/components/TagList.tsx
@@ -1,69 +1,62 @@
-import { CodeEditor, CodeEditorControl } from '@patternfly/react-code-editor';
-import {
-  DescriptionList,
-  DescriptionListDescription,
-  DescriptionListGroup,
-  DescriptionListTerm,
-} from '@patternfly/react-core';
+import { Button, TextInput } from "@patternfly/react-core";
 import {
   CheckIcon,
   PencilAltIcon,
   TagIcon,
   TimesIcon,
   TrashIcon,
-} from '@patternfly/react-icons';
+} from "@patternfly/react-icons";
 import {
   ActionsColumn,
-  ExpandableRowContent,
   IAction,
   Table,
   Tbody,
   Td,
   Tr,
-} from '@patternfly/react-table';
-import { FunctionComponent, useState } from 'react';
+} from "@patternfly/react-table";
+import { FunctionComponent, useCallback, useState } from "react";
 
-interface Tag {
+export type Tag = {
   name: string;
   description: string;
-}
+};
 
-interface TagListProps {
+export type TagListProps = {
   tagList: Tag[];
-}
+};
 
 export const TagList: FunctionComponent<TagListProps> = ({ tagList }) => {
-  const [expandedTagNames, setExpandedTagNames] = useState<string[]>([]);
   const [editingTagName, setEditingTagName] = useState<string | null>(null);
-  const [currentCode, setCurrentCode] = useState<string>('');
-  const [updatedTagList, setUpdatedTagList] = useState<Tag[]>(tagList); 
+  const [currentDescription, setCurrentDescription] = useState<string>("");
+  const [updatedTagList, setUpdatedTagList] = useState<Tag[]>(tagList);
 
-  const setTagExpanded = (tagName: string, isExpanding = true) => {
-    setExpandedTagNames((prevExpanded) => {
-      const otherExpandedTags = prevExpanded.filter((name) => name !== tagName);
-      return isExpanding ? [...otherExpandedTags, tagName] : otherExpandedTags;
-    });
-  };
+  const handleDescriptionClick = useCallback(
+    (tagName: string, description: string) => {
+      setEditingTagName(tagName);
+      setCurrentDescription(description);
+    },
+    [],
+  );
+
+  const updateDescription = useCallback(
+    (tagName: string) => {
+      const updatedTags = updatedTagList.map((tag) =>
+        tag.name === tagName
+          ? { ...tag, description: currentDescription }
+          : tag,
+      );
+      setUpdatedTagList(updatedTags);
+      setEditingTagName(null);
+    },
+    [updatedTagList, currentDescription],
+  );
+
+  const cancelEditing = useCallback(() => {
+    setEditingTagName(null);
+    setCurrentDescription("");
+  }, []);
 
   const isEditingTag = (tagName: string) => editingTagName === tagName;
-
-  const handleDescriptionClick = (tagName: string, description: string) => {
-    setEditingTagName(tagName);
-    setCurrentCode(description);
-  };
-
-  const updateDescription = (tagName: string) => {
-    const updatedTags = updatedTagList.map((tag) => {
-      if (tag.name === tagName) {
-        return { ...tag, description: currentCode };
-      }
-      return tag;
-    });
-    setUpdatedTagList(updatedTags);
-    setEditingTagName(null); 
-  };
-
-  const isTagExpanded = (tagName: string) => expandedTagNames.includes(tagName);
 
   const defaultActions = (): IAction[] => [
     {
@@ -73,7 +66,7 @@ export const TagList: FunctionComponent<TagListProps> = ({ tagList }) => {
           &nbsp;Rename
         </>
       ),
-      onClick: () => { },
+      onClick: () => {},
     },
     {
       title: (
@@ -82,80 +75,66 @@ export const TagList: FunctionComponent<TagListProps> = ({ tagList }) => {
           &nbsp;Delete
         </>
       ),
-      onClick: () => { },
+      onClick: () => {},
     },
   ];
 
-  const editCustomControl = (
-    <CodeEditorControl
-      icon={<CheckIcon color="blue" />}
-      aria-label="Save code"
-      tooltipProps={{ content: 'Save' }}
-      onClick={() => editingTagName && updateDescription(editingTagName)} 
-    />
-  );
-
-  const cancelCustomControl = (
-    <CodeEditorControl
-      icon={<TimesIcon />}
-      aria-label="Cancel edit"
-      tooltipProps={{ content: 'Cancel edit' }}
-      onClick={() => setEditingTagName(null)} 
-    />
-  );
-
   return (
     <Table aria-label="Tag List">
-      {updatedTagList.map((tag, rowIndex) => (
-        <Tbody key={tag.name} isExpanded={isTagExpanded(tag.name)}>
+      {updatedTagList.map((tag) => (
+        <Tbody key={tag.name}>
           <Tr>
-            <Td
-              expand={{
-                rowIndex,
-                isExpanded: isTagExpanded(tag.name),
-                onToggle: () =>
-                  setTagExpanded(tag.name, !isTagExpanded(tag.name)),
-                expandId: 'tag-expandable',
-              }}
-            />
             <Td dataLabel="Name">
               <>
                 <TagIcon /> {tag.name}
               </>
             </Td>
-            <Td dataLabel="Tag Description">{tag.description}</Td>
+            <Td dataLabel="Tag Description">
+              {isEditingTag(tag.name) ? (
+                <div style={{ display: "flex", alignItems: "center" }}>
+                  <TextInput
+                    value={currentDescription}
+                    onChange={(_event, value) => setCurrentDescription(value)}
+                    aria-label="Edit tag description"
+                  />
+                  <Button
+                    variant="plain"
+                    aria-label="Save description"
+                    onClick={() => updateDescription(tag.name)}
+                  >
+                    <CheckIcon />
+                  </Button>
+                  <Button
+                    variant="plain"
+                    aria-label="Cancel edit"
+                    onClick={cancelEditing}
+                  >
+                    <TimesIcon />
+                  </Button>
+                </div>
+              ) : (
+                <span
+                  onClick={() =>
+                    handleDescriptionClick(tag.name, tag.description)
+                  }
+                >
+                  {tag.description || "No description provided."}
+                  <Button
+                    variant="plain"
+                    aria-label="Edit description"
+                    onClick={() =>
+                      handleDescriptionClick(tag.name, tag.description)
+                    }
+                  >
+                    <PencilAltIcon />
+                  </Button>
+                </span>
+              )}
+            </Td>
             <Td isActionCell>
               <ActionsColumn items={defaultActions()} />
             </Td>
           </Tr>
-          {isTagExpanded(tag.name) && (
-            <Tr isExpanded={isTagExpanded(tag.name)}>
-              <Td colSpan={2}>
-                <ExpandableRowContent>
-                  <DescriptionList>
-                    <DescriptionListGroup>
-                      <DescriptionListTerm>Description</DescriptionListTerm>
-                      <DescriptionListDescription
-                        onClick={() =>
-                          handleDescriptionClick(tag.name, tag.description)
-                        }
-                      >
-                        {tag.description}
-                      </DescriptionListDescription>
-                    </DescriptionListGroup>
-                  </DescriptionList>
-                  {isEditingTag(tag.name) && (
-                    <CodeEditor
-                      height="200px"
-                      code={currentCode}
-                      onChange={(value) => setCurrentCode(value || '')}
-                      customControls={[editCustomControl, cancelCustomControl]}
-                    />
-                  )}
-                </ExpandableRowContent>
-              </Td>
-            </Tr>
-          )}
         </Tbody>
       ))}
     </Table>
