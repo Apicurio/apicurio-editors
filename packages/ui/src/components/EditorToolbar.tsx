acd77cf3ac72dd385a01ce7735268e48d4d158a2
Riccardo Forina <rforina@redhat.com>
2024-11-06 17:05:03 +0100

    Issues report

--- packages/ui/src/components/EditorToolbar.tsx
+++ packages/ui/src/components/EditorToolbar.tsx
@@ -1,24 +1,54 @@
 import {
-  Badge,
+  Button,
+  Label,
+  LabelGroup,
   Toolbar,
   ToolbarContent,
   ToolbarItem,
 } from "@patternfly/react-core";
 import { UndoRedo } from "./UndoRedo.tsx";
 import { OpenApiEditorMachineContext } from "../OpenApiEditor.tsx";
+import { groupBy } from "lodash";
+import {
+  ErrorCircleOIcon,
+  InfoIcon,
+  WarningTriangleIcon,
+} from "@patternfly/react-icons";
 
 export function EditorToolbar() {
-  const { validationMessagesCount } = OpenApiEditorMachineContext.useSelector(
-    ({ context }) => ({
-      validationMessagesCount: context.validationProblems.length,
-    })
+  const { low, medium, high } = OpenApiEditorMachineContext.useSelector(
+    ({ context }) => {
+      const groupedCount = groupBy(
+        context.validationProblems,
+        (v) => v.severity
+      );
+      console.log(groupedCount);
+      return {
+        low: groupedCount[1]?.length ?? 0,
+        medium: groupedCount[2]?.length ?? 0,
+        high: groupedCount[3]?.length ?? 0,
+      };
+    }
   );
   const actorRef = OpenApiEditorMachineContext.useActorRef();
   return (
     <Toolbar>
       <ToolbarContent>
-        <ToolbarItem alignSelf={"center"}>
-          Issues <Badge>{validationMessagesCount}</Badge>
+        <ToolbarItem alignSelf={"center"} variant={"label"}>
+          Issues&nbsp;
+          <Button variant={"plain"}>
+            <LabelGroup>
+              <Label color={"blue"} icon={<InfoIcon />}>
+                {low}
+              </Label>
+              <Label color={"yellow"} icon={<WarningTriangleIcon />}>
+                {medium}
+              </Label>
+              <Label color={"red"} icon={<ErrorCircleOIcon />}>
+                {high}
+              </Label>
+            </LabelGroup>
+          </Button>
         </ToolbarItem>
         <ToolbarItem variant="separator" />
         <ToolbarItem>
